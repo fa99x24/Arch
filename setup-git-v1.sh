e1b2ab36c916af97ef6343e85f3c6b3c1c161058
fa99x24 <thomas@batliner.eu>
2017-10-23 14:11:28 +0200

    first test

---
+++ setup-git-v1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	https://www.erikdubois.be
+# Website	:	https://www.archmerge.com
+# Website	:	https://www.archmerged.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "fa99x24"
+git config --global user.email "thomas@batliner.eu"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
